bc4f27374a28e3cdc362d2647a1a4d0373c93a3b
pawelbi1990 <114158452+pawelbi1990@users.noreply.github.com>
2024-08-20 15:56:23 +0200

    Update GoogleChatAndSlackNotifier.js

--- GoogleChatAndSlackNotifier.js
+++ GoogleChatAndSlackNotifier.js
@@ -1,50 +1,51 @@
 function onEdit(e) {
-  
-    var sheet = e.source.getActiveSheet();
-    var range = e.range;
-    var cell = range.getA1Notation();
-    var sheetUrl = e.source.getUrl()
-    
-    var configSheet = e.source.getSheetByName("Config");
-    var googleUrl = (configSheet.getRange("B2").getValue())
-    var slackUrl = (configSheet.getRange("B3").getValue())
-  
-    
-  
-  
-  
-  function slackWebhook(message, url) {   
-      var options = {
-      "method" : "post",
-      "contentType": "application/json",
-      "payload" : JSON.stringify(message)
-    };  
-    const response = UrlFetchApp.fetch(url, options);
-    Logger.log(response)
-  }
-  function googleWebhook(message, url) {
-    const options = {
-      "method": "post",
-      "headers": {"Content-Type": "application/json; charset=UTF-8"},
-      "payload": JSON.stringify({"text": message})
-    };
-    const response = UrlFetchApp.fetch(url, options);
-    Logger.log(response)
-  }
-  
-  function sendMessage(triggerSheet, trigger, message, url) {
-    if (sheet.getName() === triggerSheet && cell === trigger) {
-      if (range.getValue() === true) {
-        if (configSheet.getRange("B1").getValue() === "googlechat"){
-          googleWebhook(message, url)
-        } else if (configSheet.getRange("B1").getValue() === "slack") {
-          slackWebhook(message, url)
-        }
+
+  var sheet = e.source.getActiveSheet();
+  var range = e.range;
+  var cell = range.getA1Notation();
+  var sheetUrl = e.source.getUrl()
+
+  var configSheet = e.source.getSheetByName("Config");
+  var googleUrl = (configSheet.getRange("B2").getValue())
+  var slackUrl = (configSheet.getRange("B3").getValue())
+
+
+
+
+
+
+function slackWebhook(message, url) {
+    var options = {
+    "method" : "post",
+    "contentType": "application/json",
+    "payload" : JSON.stringify({"text": message})
+  };
+  const response = UrlFetchApp.fetch(url, options);
+  Logger.log(response)
+}
+function googleWebhook(message, url) {
+  const options = {
+    "method": "post",
+    "headers": {"Content-Type": "application/json; charset=UTF-8"},
+    "payload": JSON.stringify({"text": message})
+  };
+  const response = UrlFetchApp.fetch(url, options);
+  Logger.log(response)
+}
+
+function sendMessage(triggerSheet, trigger, message, url) {
+  if (sheet.getName() === triggerSheet && cell === trigger) {
+    if (range.getValue() === true) {
+      if (configSheet.getRange("B1").getValue() === "googlechat"){
+        googleWebhook(message, url)
+      } else if (configSheet.getRange("B1").getValue() === "slack") {
+        slackWebhook(message, url)
       }
     }
   }
-  
-  sendMessage("Triggers", "A1", "Test message", googleUrl)
-  
-  
-  }
+}
+
+sendMessage("Triggers", "A1", "Test message", slackUrl)
+
+
+}
